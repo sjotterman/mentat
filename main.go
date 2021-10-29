92da63d5364f610d2907c1cc17df5d3b3b6d3cf9
Samuel Otterman <samuel.otterman@brandingbrand.com>
2021-10-28 21:58:26 -0400

    implement lipgloss spinner

--- main.go
+++ main.go
@@ -2,8 +2,74 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
+type errMsg error
+
+type model struct {
+	spinner  spinner.Model
+	quitting bool
+	err      error
+}
+
+func initialModel() model {
+	s := spinner.NewModel()
+	s.Spinner = spinner.Dot
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	return model{spinner: s}
+}
+
 func main() {
-	fmt.Println("hello, world")
+	// Log to a file. To use, export BUBBLETEA_LOG
+	logfilePath := os.Getenv("BUBBLETEA_LOG")
+	if logfilePath != "" {
+		if _, err := tea.LogToFile(logfilePath, "simple"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Initialize
+	p := tea.NewProgram(initialModel())
+	if err := p.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return spinner.Tick
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc", "ctrl-c":
+			m.quitting = true
+			return m, tea.Quit
+		default:
+			return m, nil
+		}
+	case errMsg:
+		m.err = msg
+		return m, nil
+
+	default:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	}
+}
+
+func (m model) View() string {
+	if m.err != nil {
+		return m.err.Error()
+	}
+	str := fmt.Sprintf("\n\n%s  Loading...press q to quit\n\n", m.spinner.View())
+	return str
 }
