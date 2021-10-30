ba857eee7b08db42dc7647f4bb4ccb36c5e36b28
Samuel Otterman <samuel.otterman@brandingbrand.com>
2021-10-30 16:59:51 -0400

    hardcoded list of files

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require github.com/charmbracelet/bubbletea v0.18.0
 
 require (
+	github.com/atotto/clipboard v0.1.2 // indirect
 	github.com/charmbracelet/bubbles v0.9.0 // indirect
 	github.com/charmbracelet/lipgloss v0.4.0 // indirect
 	github.com/containerd/console v1.0.2 // indirect
@@ -16,6 +17,7 @@ require (
 	github.com/muesli/termenv v0.9.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sahilm/fuzzy v0.1.0 // indirect
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 	golang.org/x/term v0.0.0-20210422114643-f5beecf764ed // indirect
 )
--- main.go
+++ main.go
@@ -1,28 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
-	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
 type errMsg error
 
+type item struct {
+	title, desc string
+}
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
 type model struct {
-	spinner  spinner.Model
 	quitting bool
+	list     list.Model
 	err      error
 }
 
 func initialModel() model {
-	s := spinner.NewModel()
-	s.Spinner = spinner.Dot
-	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	return model{spinner: s}
+	items := []list.Item{
+		item{title: "foo.md", desc: "First file"},
+		item{title: "bar.md", desc: "Second file"},
+		item{title: "foobar.md", desc: "Third file"},
+	}
+	return model{list: list.NewModel(items, list.NewDefaultDelegate(), 0, 0)}
 }
 
 func main() {
@@ -36,40 +47,35 @@ func main() {
 
 	// Initialize
 	p := tea.NewProgram(initialModel())
+	p.EnterAltScreen()
 	if err := p.Start(); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func (m model) Init() tea.Cmd {
-	return spinner.Tick
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "esc", "ctrl-c":
-			m.quitting = true
-			return m, tea.Quit
-		default:
+		if msg.String() == "ctrl+c" {
 			return m, nil
 		}
-	case errMsg:
-		m.err = msg
-		return m, nil
-
-	default:
-		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
-		return m, cmd
+	case tea.WindowSizeMsg:
+		top, right, bottom, left := docStyle.GetMargin()
+		m.list.SetSize(msg.Width-left-right, msg.Height-top-bottom)
 	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
 	if m.err != nil {
 		return m.err.Error()
 	}
-	str := fmt.Sprintf("\n\n%s  Loading...press q to quit\n\n", m.spinner.View())
-	return str
+	return docStyle.Render(m.list.View())
 }
